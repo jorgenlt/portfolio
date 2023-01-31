24c22d1d7207e69206a43551c12febac342fdd1e
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-01-31 14:46:31 +0700

    complete curr

--- app/javascript/controllers/currencyconverter_controller.js
+++ app/javascript/controllers/currencyconverter_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="currencyconverter"
 export default class extends Controller {
-  static targets = ['fromcurrency', 'tocurrency', "amount", "result"]
+  static targets = ['fromcurrency', 'tocurrency', "amount", "result", "resultTop", "resultBottom"]
   static values = []
 
   connect() {
@@ -12,21 +12,32 @@ export default class extends Controller {
 
   convert() {
     console.log('convert executed');
+    this.resultTarget.parentNode.classList.remove("slideDown");
+    this.resultTarget.parentNode.classList.add("d-none");
     const fromCurrency = this.fromcurrencyTarget.value
     this.#convertCurrency(fromCurrency);
   }
 
   #convertCurrency(from) {
     fetch(`https://open.er-api.com/v6/latest/${from}`)
-      .then(response => response.json())
-      .then((data) => {
-        const amount = this.amountTarget.value;
-        const toCurrency = this.tocurrencyTarget.value;
-        const rate = data.rates[toCurrency];
-        const result = amount * rate;
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data.rates);
+      const amount = this.amountTarget.value;
+      const toCurrency = this.tocurrencyTarget.value;
+      const rate = data.rates[toCurrency];
+      const result = `${amount * rate} ${toCurrency}`;
 
-        this.resultTarget.parentNode.classList.remove("d-none");
-        this.resultTarget.innerHTML = result;
-      })
+      this.resultTopTarget.innerHTML = `${amount} ${from} =`
+      this.resultTarget.innerHTML = `${this.#roundOff(parseFloat(result), 2)} ${toCurrency}`;
+      this.resultBottomTarget.innerHTML = `1 ${from} = ${this.#roundOff(parseFloat(rate), 2)} ${toCurrency}`;
+      this.resultTarget.parentNode.classList.add("slideDown");
+      this.resultTarget.parentNode.classList.remove("d-none");
+    })
+  }
+
+  #roundOff(num, decimals) {
+    const x = Math.pow(10, decimals);
+    return Math.round(num * x) / x;
   }
 }
