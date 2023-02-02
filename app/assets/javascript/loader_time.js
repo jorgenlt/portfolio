58c200ced5e10615dad7f3f47adffd6ea290e6b3
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-02-02 12:40:02 +0700

    clean up js files

---
+++ app/assets/javascript/loader_time.js
@@ -0,0 +1,6 @@
+const loaderContainer = document.querySelector('.loader-container');
+
+// load animation based on a set time
+setTimeout(() => {
+  loaderContainer.classList.add('d-none')
+  }, 1300 );
