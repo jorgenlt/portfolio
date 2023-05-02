a328b15803f8c738292da05d0021c2174d934f32
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-05-02 19:39:27 +0200

    imp. fonts

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,9 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@200;400;600&family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Space+Mono&display=swap" rel="stylesheet" />
         <script src="https://cdn.jsdelivr.net/npm/pushbar.js@1.0.0/src/pushbar.min.js"></script>
       </Head>
       <body>
