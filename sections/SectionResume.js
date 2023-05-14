53fb4818256f87bd05627359980505cce23de06e
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-05-14 12:03:39 +0200

    update resume

--- sections/SectionResume.js
+++ sections/SectionResume.js
@@ -3,7 +3,7 @@ export default function SectionResume() {
         <div className="section" id="resume">
             <div id="resume-content">
                 <h1>Resume</h1>
-                <p>Download my resume <a href="/files/jorgenlt_cv_v4.pdf" target="_blank">here</a>(pdf).</p>
+                <p>Download my resume <a href="/files/jorgenlt_cv_v5.pdf" target="_blank">here</a>(pdf).</p>
             </div>
         </div>
     )
