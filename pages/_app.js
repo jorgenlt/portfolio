be3e7eef800aba554a4235981680a95a718de0ca
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-05-19 13:29:17 +0200

    ga with ga-tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect } from 'react';
-import '@/styles/app.scss'
+import Script from 'next/script';
+import '@/styles/app.scss';
 
 
 export default function App({ Component, pageProps }) {  
@@ -8,5 +9,23 @@ export default function App({ Component, pageProps }) {
     require('bootstrap/dist/js/bootstrap.bundle.min.js');
   }, []);
 
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-Y3C5HBGP1R"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-Y3C5HBGP1R);
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+    ) 
 }
