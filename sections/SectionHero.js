128ddd129dc5edfee2bb1a93e7e0f8714542e283
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-05-19 11:51:28 +0200

    cta + contact link

--- sections/SectionHero.js
+++ sections/SectionHero.js
@@ -6,12 +6,23 @@ export default function SectionHero() {
                 <h1 id="hero--title">Jørgen Larsen Tjønnteig</h1>
                 <h1 id="hero--subtitle">Full Stack Web Developer</h1>
                 <p>Full Stack Web Development, Javascript, React, Ruby, Rails, SQL, HTML, CSS, UX/UI, Bootstrap, Sass</p>
-                <div 
-                    id="hero--cta" 
-                    className="disable-select"
-                    onClick={() => window.location.href = '#2'}
-                >
-                    See my projects
+                <div id="hero--actions">
+                    <div 
+                        id="hero--cta" 
+                        className="disable-select"
+                        onClick={() => window.location.href = '#2'}
+                    >
+                        See my projects
+                    </div>
+                    <a 
+                        id="hero--contact" 
+                        className="disable-select"
+                        href="mailto:contact@jorgenlt.me"
+                        target="_blank"
+                    >
+                        Contact
+                    </a>
+
                 </div>
             </div>
             <div id="learn-more-container" onClick={() => window.location.href = '#1'}>
--- sections/SectionPortfolio.js
+++ sections/SectionPortfolio.js
@@ -14,13 +14,13 @@ const portfolioCards = [
     { id: 1, name: 'Homify', tech: ['rails', 'javascript', 'ruby'] },
     { id: 2, name: 'Messagemate', tech: ['rails', 'javascript', 'ruby', 'android'] },
     { id: 3, name: 'Smartbot', tech: ['react native', 'javascript', 'android'] },
-    { id: 4, name: 'PortfolioWebsite', tech: ['react', 'javascript'] },
+    { id: 4, name: 'PortfolioWebsite', tech: ['react', 'javascript', 'next.js'] },
     { id: 5, name: 'Spotify', tech: ['javascript'] },
     { id: 6, name: 'Findit', tech: ['rails', 'ruby', 'javascript'] },
     { id: 7, name: 'Tidder', tech: ['rails', 'javascript', 'ruby'] },
-    { id: 8, name: 'Tenzies', tech: ['react', 'javascript'] },
-    { id: 9, name: 'OtherProjects', tech: ['react', 'javascript'] },
-    { id: 10, name: 'NodejsChatgpt', tech: ['node.js', 'javascript'] },
+    { id: 8, name: 'NodejsChatgpt', tech: ['node.js', 'javascript'] },
+    { id: 9, name: 'Tenzies', tech: ['react', 'javascript'] },
+    { id: 10, name: 'OtherProjects', tech: ['react', 'javascript'] }
 ];
 
 export default function SectionPortfolio() {
@@ -50,13 +50,14 @@ export default function SectionPortfolio() {
                     <p></p>
                     <div id='portfolio--filters'>
                         <button onClick={handleTechButtonClick} value="All" className={selectedTech.length === 0 ? 'selected' : ''}>All</button>
+                        <button onClick={handleTechButtonClick} value="android" className={selectedTech.includes('android') ? 'selected' : ''}>Android</button>
+                        <button onClick={handleTechButtonClick} value="javascript" className={selectedTech.includes('javascript') ? 'selected' : ''}>JavaScript</button>
+                        <button onClick={handleTechButtonClick} value="next.js" className={selectedTech.includes('next.js') ? 'selected' : ''}>Next.js</button>
+                        <button onClick={handleTechButtonClick} value="node.js" className={selectedTech.includes('node.js') ? 'selected' : ''}>Node.js</button>
+                        <button onClick={handleTechButtonClick} value="rails" className={selectedTech.includes('rails') ? 'selected' : ''}>Rails</button>
                         <button onClick={handleTechButtonClick} value="react" className={selectedTech.includes('react') ? 'selected' : ''}>React</button>
                         <button onClick={handleTechButtonClick} value="react native" className={selectedTech.includes('react native') ? 'selected' : ''}>React Native</button>
-                        <button onClick={handleTechButtonClick} value="javascript" className={selectedTech.includes('javascript') ? 'selected' : ''}>JavaScript</button>
                         <button onClick={handleTechButtonClick} value="ruby" className={selectedTech.includes('ruby') ? 'selected' : ''}>Ruby</button>
-                        <button onClick={handleTechButtonClick} value="rails" className={selectedTech.includes('rails') ? 'selected' : ''}>Rails</button>
-                        <button onClick={handleTechButtonClick} value="android" className={selectedTech.includes('android') ? 'selected' : ''}>Android</button>
-                        <button onClick={handleTechButtonClick} value="node.js" className={selectedTech.includes('node.js') ? 'selected' : ''}>Node.js</button>
                     </div>
                 </div>
                 <div className="portfolio-cards--container">
