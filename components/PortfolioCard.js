01133b73cc404137019768b90cc3e33da55e4af5
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-05-21 21:14:21 +0200

    p-cards readability

--- components/PortfolioCard.js
+++ components/PortfolioCard.js
@@ -5,6 +5,7 @@ export default function portfolioCards(props) {
             style={{backgroundImage: `url(${props.backgroundImage})`}}
         >
             <div className='portfolio-cards--card-overlay'></div>
+            <div className='portfolio-cards--card-overlay2'></div>
             <div className="portfolio-cards--card-content">
                 <h2 className="portfolio-cards--card-title">{props.title}</h2>
                 <p>{props.description}</p>
